47b35a0a1d514b6d59118c2d2eb1005e7482637a
TarrynH <Tarrynhurrell@gmail.com>
2023-07-04 18:26:53 +0200

    changes

--- Challenge2/scripts.js
+++ Challenge2/scripts.js
@@ -3,16 +3,15 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 0;
-const minuteOfDay = 0;
+const hourOfDay = 00;
+const minuteOfDay = 00;
 
 // Only change below this line
 
-if (hourOfDay === 0 && minuteOfDay === 0)   {
-	const taxAsDecimal = parseInt(tax) / 100
-    const startingAfterTax = salary * (1 - taxAsDecimal)
+if ((hourOfDay == '00') && (minuteOfDay == '00')) {
+	const taxAsDecimal = parseInt(tax) / 100  //works the same with '100'
+    const startingAfterTax = salary * (1 - taxAsDecimal) //works the same with '1'
 	const balance = startingAfterTax - transport - food - rent
-    console.log("R" + balance.toFixed(2))
+    console.log("R "+ balance.toFixed(2))
 }
 
-	
